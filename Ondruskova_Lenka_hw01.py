d3374668d3cd66ceb34de1f59fd9f12a4e2e97de
Lenka Ondrušková <kulkova.lenka@gmail.com>
2024-04-21 14:46:29 +0200

    Formatovani

--- Ondruskova_Lenka_hw01.py
+++ Ondruskova_Lenka_hw01.py
@@ -4,15 +4,13 @@ dictionary = {}
 
 with open("alice.txt", encoding="utf-8") as inputfile:
     for line in inputfile:
-        words = line.lower().strip('\n').split(' ')
+        words = line.lower().strip("\n").split(" ")
         for word in words:
             for letter in word:
                 if letter in dictionary.keys():
                     dictionary[letter] = dictionary[letter] + 1
                 else:
                     dictionary[letter] = 1
-              
-with open("hw01_output.json", mode='w', encoding="utf-8") as file:
-    json.dump(dictionary, file, indent=4, ensure_ascii=False, sort_keys=True)
 
-    
+with open("hw01_output.json", mode="w", encoding="utf-8") as file:
+    json.dump(dictionary, file, indent=4, ensure_ascii=False, sort_keys=True)
